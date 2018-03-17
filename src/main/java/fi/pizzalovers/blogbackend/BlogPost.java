64aa02020560c19e8c70aed787a03ca77b9bef93
Gonzalo ortiz <gonzalo.ortizbianco@cs.tamk.fi>
2018-03-17 15:14:04 +0200

    Added attributes and annotations to BlogPost.java Added Autowiring to Application

--- src/main/java/fi/pizzalovers/blogbackend/BlogPost.java
+++ src/main/java/fi/pizzalovers/blogbackend/BlogPost.java
@@ -1,5 +1,53 @@
 package fi.pizzalovers.blogbackend;
 
+import javax.persistence.*;
+import java.util.Date;
 
+@Entity
+@Table(name = "blogpost")
 public class BlogPost {
+    @Id
+    @GeneratedValue
+    int id;
+    @Column(name = "title")
+    String title;
+    @Column(name = "body")
+    String body;
+    @Column(name ="date")
+    Date date;
+    @Column(name ="user")
+    String user;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 }
--- src/main/java/fi/pizzalovers/blogbackend/BlogbackendApplication.java
+++ src/main/java/fi/pizzalovers/blogbackend/BlogbackendApplication.java
@@ -1,10 +1,16 @@
 package fi.pizzalovers.blogbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+
 @SpringBootApplication
 public class BlogbackendApplication {
+
+	@Autowired
+	EntityManager entityManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogbackendApplication.class, args);
