f7c94f1878f9b756fbc8f75608066a317d5011e3
Ryhis <teemu.ryhanen@cs.tamk.fi>
2018-04-01 16:12:21 +0300

    Comment add basic stuff

--- src/main/java/fi/pizzalovers/blogbackend/Comment.java
+++ src/main/java/fi/pizzalovers/blogbackend/Comment.java
@@ -1,4 +1,23 @@
 package fi.pizzalovers.blogbackend;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name="Comments")
 public class Comment {
+    @Id
+    @GeneratedValue
+    long id;
+    @Column(name = "body")
+    String body;
+    @Column(name = "username")
+    String username;
+    @Column(name = "date")
+    Date date;
+    @Column(name = "blogpostId")
+    String blogpostId;
+
 }
